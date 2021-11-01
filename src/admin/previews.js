a6b211f8954c0b7129d6755a3cc5830e8d8e9654
Heather Migliorisi <hmig@users.noreply.github.com>
2021-10-31 22:40:59 -0400

    Update previews.js

--- src/admin/previews.js
+++ src/admin/previews.js
@@ -68,9 +68,7 @@ const TwoColumnPage = ({ entry }) => (
     path="layouts/two-column.njk"
     context={({ title, body, aside, intro }) => ({
       title,
-      content: markdownFilter(body || ''),
-      intro: markdownFilter(intro || ''),
-      aside: markdownFilter(aside || ''),
+      content: markdownFilter(body || '', aside || '', intro || ''),
     })}
   />
 );
